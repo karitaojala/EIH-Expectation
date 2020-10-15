3e62b54acf563d65bf7767792b41062e0fea28df
Karita Ojala <k.ojala@uke.de>
2020-10-15 16:17:27 +0200

    Cleaning up, implemented trialwise CPAR init/kill

--- scripts/Pain_test_long/InstantiateParameters.m
+++ scripts/Pain_test_long/InstantiateParameters.m
@@ -46,36 +46,35 @@ if P.devices.arduino
 end
 
 % stimulus parameters
-P.preExposure_intensities   = [10 20 30 40 50]; % preexposure pressure intensities (kPa) to test rough ballpark of pain threshold of the participant
+P.pain.preExposure          = [10 20 30 40 50]; % preexposure pressure intensities (kPa) to test the rough ballpark of pain threshold of the participant
 P.test_range                = 10.0:1:90.0; % possible pressure range (kPa) for pressure pain test
 P.test_start                = 30; % starting value (kPa); may be overridden after preexposure check
 P.test_step                 = 3;  % +- pressure change (kPa) from starting value or pressure pain test
 P.test_trials               = 7;  % number of trials for the pressure test
 P.test_repeats              = 3;  % number of times the whole set of stimuli is repeated (with different randomized order)
 
-P.pain.rS                           = 15; % rise speed
+P.pain.rS                            = 15; % rise speed
 
-P.presentation.cueing               = 1; % whether pain stimuli and others will be cued (typically by white/red cross)
-P.painCalibData.PeriThrN            = 1; % turn on fixation cross for first stimulus
-P.presentation.initialTrials        = [1 1+P.test_trials 1+P.test_trials*2];
-P.presentation.sStimPlateauPreexp   = 60; % duration of the constant pressure plateau after rise time for pre-exposure (part 1)
-P.presentation.sStimPlateau         = 60; % duration of the constant pressure plateau after rise time for pressure test (part 2)
-P.presentation.sPreexpITI           = 20; % pre-exposure ITI
-P.presentation.sPreexpCue           = P.presentation.sStimPlateauPreexp/P.pain.rS+P.presentation.sStimPlateauPreexp; % pre-exposure cue duration (approximate stimulus duration with rise time included)
-P.presentation.sMinMaxThreshITIs    = [20 20]; % seconds between stimuli; will be randomized between two values - to use constant ITI, use two identical values
-P.presentation.sMinMaxThreshCues    = [0.5 0.5]; % jittered time prior to the stimulus that the white cross turns red; can be [0 0] (to turn off red cross altogether), but else MUST NOT BE LOWER THAN 0.5
-P.presentation.firstThresholdITI    = 20;
-P.presentation.firstPlateauITI      = 20;
-P.presentation.firstPlateauCue      = P.presentation.sStimPlateau/P.pain.rS+P.presentation.sStimPlateau;
+%P.presentation.cueing                = 1; % whether pain stimuli and others will be cued (typically by white/red cross)
+P.presentation.initialTrials         = [1 1+P.test_trials 1+P.test_trials*2];
+P.presentation.sStimPlateauPreexp    = 60; % duration of the constant pressure plateau after rise time for pre-exposure (part 1)
+P.presentation.sPreexpITI            = 30; % pre-exposure ITI
+P.presentation.sPreexpCue            = P.presentation.sStimPlateauPreexp/P.pain.rS+P.presentation.sStimPlateauPreexp; % pre-exposure cue duration (stimulus duration with rise time included)
+P.presentation.sStimPlateau          = 60; % duration of the constant pressure plateau after rise time for pressure test (part 2)
+P.presentation.pressureTestITI       = 20; % pressure test ITI
+P.presentation.pressureTestReturnVAS = 10; % time to return VAS to zero
+P.presentation.pressureTestTotalITI  = 30; % total ITI between trials
+P.presentation.pressureTestBlockStop = 2;  % time to stop at the block display
+%P.presentation.pressureTestCue       = P.presentation.sStimPlateau/P.pain.rS+P.presentation.sStimPlateau;
 
-P.presentation.sMaxRating           = 8; % Presentation duration of rating scale
+P.presentation.sMaxRating            = 8; % Presentation duration of rating scale
 
-P.pain.cuff_on                      = 1; % which cuff is used for pain (1: left, 2: right - depends on how cuffs plugged into the CPAR unit and put on participant's arm/leg)
-P.pain.cuff_off                     = 2; % the other cuff off (only use 1 cuff)
-P.pain.repeat                       = 1; % number of repeats of each stimulus
+P.pain.cuff_on                       = 1; % which cuff is used for pain (1: left, 2: right - depends on how cuffs plugged into the CPAR unit and put on participant's arm/leg)
+P.pain.cuff_off                      = 2; % the other cuff off (only use 1 cuff)
+P.pain.repeat                        = 1; % number of repeats of each stimulus
 
-P.cpar.forcedstart                  = true; % CPAR starts even with VAS not at 0 (otherwise false)
-P.cpar.stoprule                     = 'b';  % CPAR stops only at button press (not when VAS reaches the maximum, 'v')
+P.cpar.forcedstart                   = true; % CPAR starts even with VAS not at 0 (otherwise false)
+P.cpar.stoprule                      = 'b';  % CPAR stops only at button press (not when VAS reaches the maximum, 'v')
 
 end
 
